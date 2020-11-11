2448b6937727660300f1de3e79aa25239dde3c2f
t-omiya <t-omiya@fores.jp>
2020-11-11 19:06:52 +0900

    Vue : v3 の emitsプロパティを検証中。

--- Vue/1-10/2/2.js
+++ Vue/1-10/2/2.js
@@ -1,22 +1,28 @@
-Vue.component("my-comp", {
-  template: '<p class="static">この場合、static（背景青） + 文字色赤</p>',
-});
+const myData = {
+  data() {
+    return {
+      posts: [
+        { id: 1, title: 'my Title1', date: '2020/01/01', color: 'blue', size: 'big' },
+        { id: 2, title: 'bbb', date: '2020/12/12', color: 'red', size: 'medium' },
+        { id: 3, title: 'ccccccccccccccc', date: '1999/11/11', color: 'green', size: 'small' },
+      ],
+      postFontSize: 1
+    }
+  }
+}
 
-var v = new Vue({
-  el: "#el",
-  data: {
-    radio: "",
-    checked: true,
-    selected: "",
-    check2: true,
-    radio2: "",
-    b: "b1b1b1b1b1b1",
-    c: "c1c1c1c1c1c1",
-    select2: "",
-    msg1: "",
-    msgx: "",
-    msg2: "",
-    msg3: "",
+// Create a Vue application
+const app = Vue.createApp(myData)
 
-  },
-});
+app.component('bp2', {
+  props: ['post'],
+  emits: ['enlarge-text'],
+  template: `
+  <div class="bp2">
+    <h3>{{ post.title }}</h3>
+    <div v-html="post.date"></div>
+    <button @click="$emit('enlarge-text')"> Enlarge text </button>
+  </div>`
+})
+
+app.mount('#el')
