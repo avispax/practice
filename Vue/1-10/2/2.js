30d8d9fdf0519ad7f31ad0c055808b03f36181a0
t-omiya <t-omiya@fores.jp>
2020-11-12 19:03:53 +0900

    Vue : v3 コンポーネントとv-modelのなんか

--- Vue/1-10/2/2.js
+++ Vue/1-10/2/2.js
@@ -6,13 +6,31 @@ const myData = {
         { id: 2, title: 'bbb', date: '2020/12/12', color: 'red', size: 'medium' },
         { id: 3, title: 'ccccccccccccccc', date: '1999/11/11', color: 'green', size: 'small' },
       ],
-      postFontSize: 1
+      postFontSize: 1,
+      postFontSize2: 9,
+      postFontSize3: 9
     }
+  },
+  methods: {
+    myFunc(aaa) {
+      this.postFontSize3 += aaa
+    },
   }
 }
 
 // Create a Vue application
 const app = Vue.createApp(myData)
+
+app.component('blog-post', {
+  props: ['post'],
+  emits: ['enlarge-text'],
+  template: `
+  <div class="blog-post">
+    <h3>{{ post.title }}</h3>
+    <div v-html="post.date"></div>
+    <button @click="$emit('enlarge-text')"> Enlarge text </button>
+  </div>`
+})
 
 app.component('bp2', {
   props: ['post'],
@@ -21,8 +39,18 @@ app.component('bp2', {
   <div class="bp2">
     <h3>{{ post.title }}</h3>
     <div v-html="post.date"></div>
-    <button @click="$emit('enlarge-text')"> Enlarge text </button>
+    <button @click="$emit('enlarge-text', 1)"> Enlarge text </button>
   </div>`
+})
+
+app.component('bp3', {
+  props: ['post'],
+  template: `
+  <div class="bp3">
+    <h3>{{ post.title }}</h3>
+    <div v-html="post.date"></div>
+    <button @click="$emit('enlarge-text', 1)"> Enlarge text </button>
+  </div>`,
 })
 
 app.mount('#el')
